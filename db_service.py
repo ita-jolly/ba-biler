f0087c299e806dca5fd82db07a877108b9288713
SushiGirrl <lykkeflor@gmail.com>
2024-11-27 20:45:41 +0100

    Made a development branch. Crreated biler database. GET request to get all biler. generate 100 biler in the database.

--- db_service.py
+++ db_service.py
@@ -1,5 +1,7 @@
 import sqlite3
 import os
+import random
+import string
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +10,70 @@ db_path = os.getenv('DB_PATH')
 
 
 def init():
-    """Initialize the guests table if it doesn't exist."""
+    """Initialize the "biler" table if it doesn't exist."""
     with sqlite3.connect(db_path) as con:
         cur = con.cursor()
-        cur.execute('''
-        CREATE TABLE HERE
+        cur.execute('''CREATE TABLE IF NOT EXISTS biler(
+                    nummerplade TEXT PRIMARY KEY,
+                    bil_type TEXT,
+                    maerke TEXT,
+                    udlejnings_status BOOLEAN,
+                    abonnement_pris DOUBLE)
         ''')
+    con.commit()
+
+bil_maerker = ["Toyota", "Ford", "BMW"]
+abonnement_priser = [5499.00, 6699.00, 7999.00]
+
+# Function to generate random nummerplader
+def generate_nummerplade():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+# Generate 100 unique biler
+def generate_biler(num_biler=100):
+    biler = []
+    nummerplader = set()
+    
+    while len(biler) < num_biler:
+        nummerplade = generate_nummerplade()
+        if nummerplade not in nummerplader:
+            nummerplader.add(nummerplade)
+            bil_type = random.choice(["Petrollium", "Diesel", "Elektrisk", "Hybrid"])
+            maerke = random.choice(bil_maerker)
+            udlejnings_status = random.choice([True, False])
+            abonnement_pris = random.choice(abonnement_priser)
+            
+            biler.append((nummerplade, bil_type, maerke, udlejnings_status, abonnement_pris))
+    return biler
+
+# Insert generated biler into the database
+def insert_biler_to_db(biler):
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.executemany('''
+            INSERT INTO biler (nummerplade, bil_type, maerke, udlejnings_status, abonnement_pris)
+            VALUES (?, ?, ?, ?, ?)
+        ''', biler)
+        con.commit()
+
+# Generate biler and insert them into the database
+biler = generate_biler(100)
+insert_biler_to_db(biler)
+
+
+def get_biler():
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM biler')
+        rows = cur.fetchall()
+
+        all_biler = [{'nummerplade': row[0], 
+                      'bil_type': row[1], 
+                      'maerke': row[2], 
+                      'udlejnings_status': row[3], 
+                      'abonnement_pris': row[4]} 
+                      for row in rows]
+        if len(all_biler) == 0:
+            return None
+
+        return all_biler
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import db_service
 from flasgger import Swagger, swag_from
 from dotenv import load_dotenv
@@ -17,11 +17,21 @@ db_service.init()
 def index():
   return "Welcome to API"
 
+@app.route('/biler', methods=['GET'])
+@swag_from('swagger/get_biler.yml')
+def get_biler():
+    biler = db_service.get_biler()
+
+    if biler is None:
+        response = make_response({'message': 'Ingen biler fundet'}, 404)
+    else:
+        response = make_response(biler, 200)
+
+    return response
+
 
 # @app.route('/gettemplate', methods=['GET'])
 # @swag_from('swagger/get_template.yml')
-# def get_guests():
-#  return "got template"
 
 if __name__ == '__main__':
     app.run()
